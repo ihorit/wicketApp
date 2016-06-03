ed89d68be0a16b743f66f919d22a4e7b80304ee3
ihorit <ihor.tymkiv.it@gmail.com>
2016-06-04 00:24:22 +0300

    new

---
+++ src/main/java/com/wicketproject/pages/admin/ContactPage.java
@@ -0,0 +1,12 @@
+package com.wicketproject.pages.admin;
+
+import com.wicketproject.BasePage;
+import com.wicketproject.MenuItemEnum;
+
+public class ContactPage extends BasePage {
+
+    @Override
+    public MenuItemEnum getActiveMenu() {
+        return MenuItemEnum.CONTACT;
+    }
+}
---
+++ src/main/java/com/wicketproject/pages/student/ClientsPage.java
@@ -0,0 +1,12 @@
+package com.wicketproject.pages.student;
+
+import com.wicketproject.BasePage;
+import com.wicketproject.MenuItemEnum;
+
+public class ClientsPage extends BasePage {
+
+    @Override
+    public MenuItemEnum getActiveMenu() {
+        return MenuItemEnum.CLIENTS;
+    }
+}
